2d704cf8575db55ca70a32902f58ebb2e36629ae
Guoli Lv <guoli-lv@hotmail.com>
2017-06-18 11:03:52 +0800

    GUI serial device selection

--- kivy/main.py
+++ kivy/main.py
@@ -4,26 +4,79 @@ from __future__ import print_function,unicode_literals,with_statement,division
 import matplotlib
 matplotlib.use('module://kivy.garden.matplotlib.backend_kivy')
 from matplotlib import pyplot as plt
-import matplotlib.animation as animation
+#import matplotlib.animation as animation
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.popup import Popup
 from kivy.clock import Clock
 from kivy.properties import StringProperty,ListProperty
 
-import serial,struct,logging
+import serial,struct,logging,serial.tools.list_ports
+from serial import SerialException
 import numpy as np
+from scipy import signal
 
+# Kivy Material Design
 from kivymd.theming import ThemeManager
 
 __author__ = 'Guoli Lv'
 __email__ = 'guoli-lv@hotmail.com'
 
+class SerialPortSelection(BoxLayout):
+
+    """ Select Serial Port on GUI """
+
+    def __init__(self,**kwargs):
+        super(SerialPortSelection,self).__init__(**kwargs)
+        # kv file is load after rendering. So in __init__ it's impossible to access widget by ids for no widget has been renderred. Then we can use Clock.schedule_once(..., 0) to schedule the call to a function relying on ids.
+        Clock.schedule_once(self.scanPorts,0)
+        Clock.schedule_once(self.popupHintOnConnectionFailedConfig,0)
+
+
+    def popupHintOnConnectionFailedConfig(self,dt=0):
+        # Connection Failed popup hint configuration
+        self.popup = Popup(title='Connection Failed',id='popup')
+        App.get_running_app().root.add_widget(self.popup)
+        App.get_running_app().root.remove_widget(self.popup)
+
+    def changeState(self,state):
+        if state == 'down':
+            con = App.get_running_app().connect(self.ids['uart'].text)
+            # When connection failed
+            if con is False:
+                self.ids['connect'].state = 'normal'
+                # Popup hint and rescan serial devices
+                self.popup.open()
+                Clock.schedule_once(self.popup.dismiss ,1)
+                self.scanPorts()
+        else:
+            data = App.get_running_app().disconnect()
+
+    def scanPorts(self,dt=0):
+        # scan ports
+        self.ports = serial.tools.list_ports.comports()
+
+        # Clear device lists
+        self.ids['uart'].values = []
+
+        self.ids['uart'].sync_height = True
+
+        # Put ports found on selector
+        if len(self.ports) == 0:
+            self.ids['uart'].text = 'None'
+        else:
+            for port in self.ports:
+                # The first one should be put on Spinner.text else spinner will show None
+                if port is self.ports[-1]:
+                    self.ids['uart'].text = port.device
+
+                self.ids['uart'].values.append(port.device)
+
 
 class FFT(BoxLayout):
-    """Docstring for FFT. """
-    fps = 30
+    """ Real Time frequency-domain Plotting """
 
-    length = 80 # FFT length
+    length = 128 # FFT length
     fs = 125 # FFT Sampling rate
     ts = 1.0/fs # Sampling interval
     fftLen = 1024
@@ -31,49 +84,129 @@ class FFT(BoxLayout):
 
     def __init__(self,**kwargs):
         super(FFT,self).__init__(**kwargs)
+
+        self.fps = App.get_running_app().fps
+
         # Configure real time fft figure with matplotlib
         self.fig, self.ax = plt.subplots()
-        #plt.ion()
+        #plt.ion() #Never turn this on. Or enjoy a almost frozen Application.
 
-        #self.fig.subplots_adjust(bottom=0.2,left=0.3)
+        # Settings
         self.ax.set_xlabel('Frequency(Hz)')
-        self.ax.set_ylabel('Amplitude(mV)')
-        self.ax.set_xlim([0,40])
-        self.ax.set_ylim([0,1000])
+        self.ax.set_ylabel('Amplitude(uV)')
         self.ax.set_title('FFT')
 
+        # x-axis, ts = 1/fs
+        self.f = np.fft.fftfreq(self.fftLen,self.ts)
+
+        fPos = self.f[self.f>=0]
+        fPlot = [fPos[i]  for i in range(len(fPos)) if np.mod(i,self.plotScale) == 0 ]
+
+        # Plot x data once. Then we only need to update y data
+        self.ax.set_xlim([np.min(fPlot),np.max(fPlot)*1.1])
+        self.FFTplot, = self.ax.plot(fPlot,np.zeros_like(fPlot))
+
         self.add_widget(self.fig.canvas)
+
+    def start(self):
+        Clock.unschedule(self.refresh)
         Clock.schedule_interval(self.refresh,1/self.fps)
+
+    def stop(self):
+        Clock.unschedule(self.refresh)
+        self.FFTplot.set_ydata(np.zeros(self.fftLen))
+        self.ax.set_ylim([-1,1])
+        plt.draw()
 
     def refresh(self,dt):
         App.get_running_app().data.extend(App.get_running_app().readFromSerial())
         if len(App.get_running_app().data) >= self.length:
             logging.info("Refreshing. Length of data:%d"%(len(App.get_running_app().data)))
             # Clear
-            self.ax.cla()
+            #self.ax.cla()
 
             # Get data
-            x = App.get_running_app().data[0:self.length+1]
+            y = App.get_running_app().data[0:self.length+1]
 
             # Perform 512 point FFT to improve resolution
-            Y = np.fft.fft(x,self.fftLen)
-
-            # x-axis, ts = 1/fs
-            f = np.fft.fftfreq(self.fftLen,self.ts)
+            Y = np.fft.fft(y,self.fftLen)
 
             # deal with amplitude scale and choose the positive half
             #Yamp = np.abs(Y/self.fftLen)
             #YampHalf = Yamp[f>=0]
-            fPos = f[f>=0]
-            YampPos = np.abs(Y[f>=0]/self.fftLen)
+            YampPos = np.abs(Y[self.f>=0]/self.fftLen)
             YampPos[1:-1] = YampPos[1:-1] * 2
 
-            fPlot = [fPos[i]  for i in range(len(fPos)) if np.mod(i,self.plotScale) == 0 ]
             YPlot = [YampPos[i]  for i in range(len(YampPos)) if np.mod(i,self.plotScale)==0 ]
 
-            self.ax.plot(fPlot,YPlot)
+            self.FFTplot.set_ydata(YPlot)
+            padding = (np.max(YPlot) - np.min(YPlot)) * 0.1
+            if padding > 200:
+                self.ax.set_ylim([np.min(YPlot)-padding,np.max(YPlot)+padding])
+            plt.draw()
+            #self.ax.plot(fPlot,YPlot)
             # Update stored data from BCI device
             App.get_running_app().data = App.get_running_app().data[self.length:]
+
+class RealTimePlotting(BoxLayout):
+
+    fs = 125
+    length = 125 * 10
+    """Real Time time-domain Plotting """
+
+    def __init__(self,**kwargs):
+        super(RealTimePlotting ,self).__init__(**kwargs)
+        self.fps = App.get_running_app().fps
+
+        # Configure real time fft figure with matplotlib
+        self.fig, self.ax = plt.subplots()
+        #plt.ion() #Never turn this on. Or enjoy a almost frozen Application.
+
+        # Settings
+        #self.ax.set_xlabel('Time(seconds)')
+        self.ax.set_ylabel('Amplitude(uV)')
+        self.ax.get_xaxis().set_visible(False)
+        #self.ax.set_title('Real Time Plotting')
+
+        # Plot x data once. Then we only need to update y data
+        x = np.arange(0,self.length)/self.fs
+        self.ax.set_xlim([np.min(x),np.max(x)])
+        self.RealTimePlot, = self.ax.plot([],[])
+        self.RealTimePlot.set_xdata(x)
+        self.RealTimePlot.set_ydata(np.zeros_like(x).tolist())
+
+        self.add_widget(self.fig.canvas)
+
+    def start(self):
+        Clock.unschedule(self.refresh)
+        Clock.schedule_interval(self.refresh,1/self.fps)
+
+    def stop(self):
+        Clock.unschedule(self.refresh)
+        self.RealTimePlot.set_ydata(np.zeros(self.length))
+        self.ax.set_ylim([-1,1])
+        plt.draw()
+
+    def refresh(self, dt):
+        # TODO Now real time plotting and FFT cannot be showed on the same time
+        data = App.get_running_app().readFromSerial()
+
+        if data is None:
+            data=[]
+            logging.info("Nothong from serial. Please check UART connection.")
+        y = self.RealTimePlot.get_ydata()
+
+        newLen = len(y) + len(data)
+        dump = newLen - self.length
+        if dump > 0:
+            y = y[dump:]
+        y.extend(data)
+
+        self.RealTimePlot.set_ydata(y)
+        padding = (np.max(y) - np.min(y)) * 0.1
+        if padding > 200:
+            self.ax.set_ylim([np.min(y)-padding,np.max(y)+padding])
+        plt.draw()
 
 class Test(BoxLayout):
     """Test Layout"""
@@ -96,23 +229,39 @@ class Test(BoxLayout):
 class BCIApp(App):
     # Settings
     kv_directory = 'ui_template'
+    fps = 30
     data = ListProperty([])
     # Buffer
     rawRemained = StringProperty(str('')) # raw Data from serial, this should contain the data unused last time
+    ser = None
 
 
-    def __init__(self, **kwargs):
+    def __init__(self,**kwargs):
         """ Initializing serial
         :returns: TODO
         """
         super(BCIApp,self).__init__(**kwargs)
+
+    def connect(self,port='/dev/ttyUSB0'):
         try:
             # serial part
-            # TODO allow users to choose serial device
             # TODO serial should be opened after face recognition finished
-            self.ser = serial.Serial(port = '/dev/ttyUSB1',baudrate = 38400, bytesize=8,parity='N',stopbits=1)
-        except Exception as e:
-            self.ser = None
+            self.ser = serial.Serial(port = port,baudrate = 38400, bytesize=8,parity='N',stopbits=1)
+            # enable real time time-domain or/and frequency-domain plotting
+            self.root.ids['RealTimePlotting'].start()
+
+            return True
+        except SerialException:
+            # Failed
+            return False
+
+    def disconnect(self):
+        try:
+            self.ser.close()
+            self.root.ids['RealTimePlotting'].stop()
+        except AttributeError:
+            # self.ser is None. We have not connect to any available serial device
+            pass
 
     def build(self):
         return Test()
@@ -127,7 +276,7 @@ class BCIApp(App):
         raw = np.array(rawData)
         # 2.42 is the referrence voltage of BCI device, 24 is the sampling resolution
         dataVol = 2.42 / 2**24 / gainCoefficient * raw
-        dataVol = dataVol * 1000 # convert uints to mV
+        dataVol = dataVol * 1e6 # convert uints to uV
         return tuple(dataVol)
 
     def __dataHex2int(self, dataHex):
@@ -140,7 +289,7 @@ class BCIApp(App):
         data = struct.unpack('<i', dataHex)[0]
         return data
 
-    def readFromSerial(self,ser=None,protocol='EasyBCISingleChannel'):
+    def readFromSerial(self,protocol='EasyBCISingleChannel'):
         """TODO: Docstring for readFromSerial.
         :protocol:protocol type. Should be 'EasyBCISingleChannel'.
         :ser: Serial Object
@@ -152,8 +301,8 @@ class BCIApp(App):
             logging.error("Serial device not set.")
             return None
 
-        if ser is None:
-            ser = self.ser
+        ser = self.ser
+
         # Read cureent all available data
         rawData = ser.read_all()
 
